3ae56ac4b7ce2e99c416469f4fb5fde2328d6569
1inss <linus.hakegard@gmail.com>
2022-12-31 10:12:55 +0100

    fixed lady interaction

--- Group_8_Mental_health_system_for_young_women_in_Sweden_with_personalized_virtual_reality/Assets/Scripts/GameManager.cs
+++ Group_8_Mental_health_system_for_young_women_in_Sweden_with_personalized_virtual_reality/Assets/Scripts/GameManager.cs
@@ -7,7 +7,7 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
-    public event Action gameStarted, ladyInteraction, ladyMeetDialogue, plankPickedUp, sceneFinished;
+    public event Action gameStarted, ladyInteraction, ladyLineFinished, plankPickedUp, sceneFinished;
 
     private void Awake()
     {
@@ -20,31 +20,35 @@ public class GameManager : MonoBehaviour
     {
       
     }
-
+    //Called when starting game
     public void StartGame()
     {
         Debug.Log("Started game");
         gameStarted?.Invoke();
     }
 
+    //Called when a line has been spoken by lady
+    public void NextLineLady()
+    {
+        Debug.Log("talking to lady");
+        ladyLineFinished?.Invoke();
+    }
+
+    //Called after a dialogue with the lady
     public void TalkedToLady()
     {
         Debug.Log("lady interaction");
         ladyInteraction?.Invoke(); 
     }
 
-    public void NextLineLady()
-    {
-        Debug.Log("talking to lady");
-        ladyMeetDialogue?.Invoke();
-    }
-
+    //Called when a plank is picked up
     public void PlankPickedUp()
     {
         Debug.Log("Picked up plank");
         plankPickedUp?.Invoke();
     }
 
+    //Called when the scene is ending
     public void EndScene()
     {
         Debug.Log("Scene finished");
--- Group_8_Mental_health_system_for_young_women_in_Sweden_with_personalized_virtual_reality/Assets/Scripts/Lady.cs
+++ Group_8_Mental_health_system_for_young_women_in_Sweden_with_personalized_virtual_reality/Assets/Scripts/Lady.cs
@@ -6,59 +6,107 @@ using TMPro;
 
 public class Lady : MonoBehaviour
 {
-    int[] array = new int[]{1, 2, 3, 4, 5};
-    int dialogueIteration = 0;
+    //Variables used to figure out distance from player to lady
+    readonly private int CLOSETOLADYRADIUS = 3;
+    private bool closeToLady;
+    Vector3 PlayerPos;
+    Vector3 LadyPos;
 
-    Rigidbody rb;
+    //The narrative
+    string[] firstDialogueLines = new string[]{
+    "Hello!", 
+    "Could you please help me save my cat? He is stuck in a tree.", 
+    "To get him down we will need a ladder.", 
+    "But first we need to collect some planks.", 
+    "Come back to me when you have found them.",
+    "Good luck!"};
 
+    //Keeping track of the state of the first dialogue
+    int FirstLadyDialogueLength;
+    int firstDialogueIteration;
+    
+    //The ladys text variables
     public TMP_Text interactionText;
+    public TMP_Text helpText;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();  
+        //Sets state variables of the first dialogue
+        firstDialogueIteration = 0;
+        FirstLadyDialogueLength = firstDialogueLines.Length;
+
+        //Enable/Disable text
         interactionText.enabled = false;
-        GameManager.instance.ladyInteraction += StartLadyDialogueCoroutine;
-        //StartLadyDialogueCoroutine();
-       // GameManager.instance.ladyMeetDialogue += StartLadyDialogueCoroutine;
+        helpText.enabled = true;
+
+        //Subrcribing to ladyLineFinished 
+        GameManager.instance.ladyLineFinished += StartLadyDialogueCoroutine;
+
+        //Starting the first lady dialogue
+        StartLadyDialogueCoroutine();
     }
 
+    //Checks if the player is within the "CLOSETOLADYRADIUS"
     void Update()
     {
-        
-    }
+        PlayerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        LadyPos = GameObject.FindGameObjectWithTag("Lady").transform.position;
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {   
-            GameManager.instance.TalkedToLady();  
+        if(Mathf.Abs(PlayerPos.x - LadyPos.x) < CLOSETOLADYRADIUS && Mathf.Abs(PlayerPos.z - LadyPos.z) < CLOSETOLADYRADIUS)
+        {
+            closeToLady = true;
+            helpText.color = new Color(255, 255, 0, 50);
+            helpText.text = "Press F to interact or continue dialogue with lady";
+        }
+        else
+        {
+            closeToLady = false;
+            helpText.color = new Color(255, 0, 0, 50);
+            helpText.text = "Walk closer to interact with lady";
         }
     }
 
+    //Starts the first dialogue with the lady
     void StartLadyDialogueCoroutine()
     {
-        StartCoroutine(FirstLadyDialogue(dialogueIteration));
+        StartCoroutine(FirstLadyDialogue());
     }
 
-    private IEnumerator FirstLadyDialogue(int dialogueIteration)
+    //Used to busy wait while interacting with the lady, called by GameManager after first call.
+    private IEnumerator FirstLadyDialogue()
     {
-        print("början");
-        //interactionText.text = "Hello! \n \n (press F to continue)";
-        interactionText.text = array[dialogueIteration].ToString();
-        interactionText.enabled = true;
-
-        while (!Input.GetKeyDown(KeyCode.F))
+        //Waits for the player to be close to the lady and for F to be pressed
+        while (!Input.GetKeyDown(KeyCode.F) || closeToLady == false)
         {
             yield return null;
         }
 
-        interactionText.enabled = false;
-        this.dialogueIteration = this.dialogueIteration + 1;
-        print(this.dialogueIteration);
-        /*
-        if(this.dialogueIteration < 5)
+        //Adjusting variables
+        interactionText.enabled = true;
+        interactionText.text = firstDialogueLines[firstDialogueIteration];
+        firstDialogueIteration = firstDialogueIteration + 1;
+        
+        //Checks if the dialogue is finished or not
+        if(this.firstDialogueIteration < FirstLadyDialogueLength)
         {
-            GameManager.instance.NextLineLady();
-        }*/
-    }   
+            Invoke("NotifyGameManager", 0.2f);
+        }
+        else
+        {
+            IterationFinished();
+        }
+    }
+
+    //Called after each whole iteration with the lady
+    private void IterationFinished()
+    {
+        helpText.enabled = false;
+        GameManager.instance.TalkedToLady();
+    }
+    
+    //Tells the GameManager that we want to continue our conversation with the lady
+    private void NotifyGameManager()
+    {
+        GameManager.instance.NextLineLady();
+    }
 }
